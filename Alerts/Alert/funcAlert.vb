4b2b84bdc47d5a98f937440b64bb76dee9d14aed
SMR-sassa <samrufino13@gmail.com>
2020-07-23 10:09:27 -0300

    função principal

--- Alerts/Alert/funcAlert.vb
+++ Alerts/Alert/funcAlert.vb
@@ -1,7 +1,15 @@
 ﻿Public Class funcAlert
-    Function showAlert(msg As String)
-        warningAlert.msg = msg
-        warningAlert.Show()
+    Function showAlert(alert As String, msg As String)
+        If alert = "success" Then
+            successAlert.msg = msg
+            successAlert.Show()
+        ElseIf alert = "warning" Then
+            warningAlert.msg = msg
+            warningAlert.Show()
+        ElseIf alert = "danger" Then
+            dangerAlert.msg = msg
+            dangerAlert.Show()
+        End If
     End Function
 End Class
 
