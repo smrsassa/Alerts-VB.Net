0ee38e874346ef4cb8678c05126c38e41ebeda82
SMR-sassa <samrufino13@gmail.com>
2020-07-23 10:10:22 -0300

    form para testar os alertas

--- Alerts/Alert/testAlert.Designer.vb
+++ Alerts/Alert/testAlert.Designer.vb
@@ -22,46 +22,55 @@ Partial Class testAlert
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_success = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'btn_success
         '
-        Me.Button1.Location = New System.Drawing.Point(139, 61)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(132, 47)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_success.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(174, Byte), Integer), CType(CType(96, Byte), Integer))
+        Me.btn_success.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_success.ForeColor = System.Drawing.Color.White
+        Me.btn_success.Location = New System.Drawing.Point(30, 27)
+        Me.btn_success.Name = "btn_success"
+        Me.btn_success.Size = New System.Drawing.Size(132, 47)
+        Me.btn_success.TabIndex = 0
+        Me.btn_success.Text = "Success"
+        Me.btn_success.UseVisualStyleBackColor = False
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(505, 61)
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(156, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(206, 27)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(132, 47)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Text = "Warning"
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(139, 224)
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(30, 113)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(132, 47)
         Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Text = "Danger"
+        Me.Button3.UseVisualStyleBackColor = False
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(505, 224)
+        Me.Button4.Location = New System.Drawing.Point(206, 113)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(132, 47)
         Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "Alerta Padrão"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'testAlert
@@ -69,18 +78,18 @@ Partial Class testAlert
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(127, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(166, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(372, 199)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btn_success)
         Me.Name = "testAlert"
         Me.Text = "testAlert"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_success As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
--- Alerts/Alert/testAlert.vb
+++ Alerts/Alert/testAlert.vb
@@ -1,7 +1,15 @@
 ﻿Public Class testAlert
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim alert As New funcAlert
-
-        alert.showAlert("Atualizado com sucesso")
+    Private alert As New funcAlert
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_success.Click
+        alert.showAlert("success", "Atualizado com sucesso")
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        alert.showAlert("warning", "Atualizado com sucesso")
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        alert.showAlert("danger", "Atualizado com sucesso")
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox("teste")
     End Sub
 End Class
